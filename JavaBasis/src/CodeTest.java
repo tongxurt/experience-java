ce7ebe9a64d119fb0f291c1999422784322c2726
tongxu01 <tongxu01@baidu.com>
2017-08-24 20:52:40 +0800

    spring

---
+++ JavaBasis/src/CodeTest.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class CodeTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+
+            int n = scanner.nextInt();
+            int m = scanner.nextInt();
+            int k = scanner.nextInt();
+
+        }
+    }
+}
